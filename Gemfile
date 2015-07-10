f7602449aa5a3f2901ef91ec20175a85f9c44df1
andreasbloomquist <andreasbloomquist@gmail.com>
2015-07-10 12:07:30 -0700

    add some stuff

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ ruby '2.2.2'
 gem 'dotenv-rails'
 
 gem 'twilio-ruby'
-
+gem 'foundation-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.2'
 # Use postgresql as the database for Active Record
--- app/helpers/sms_helper.rb
+++ app/helpers/sms_helper.rb
@@ -1,39 +1,56 @@
 module SmsHelper
 	
-	@question_one = "First, where are you looking- give me one or more neighborhoods: SOMA, FiDi, the Mission, Jackson Square"
-	@question_two = "Ok, and how many people do you need space for?"
-	@question_three = "Do you want a creative/tech or more traditional feel?"
-	@question_four = "And when do you need it by? Plz give me a date ex: '9/1/15'"
-	@question_five = "Great! Finally, respond with any notes or special requests and my brokers will get right on this!"
-
 	def new_user?(number)
 		return true if User.find_by_phone_number(number) == nil
 	end
 
+	def create_log(params)
+		SmsLog.create({
+			:from => params[:From],
+			:to => params[:To],
+			:body => params[:Body],
+			:sms_id => params[:SmsSid]
+			})
+	end
+
+	def check_question(number)
+		@user = User.find_by_phone_number(number)
+		@user_lead = @user.leads.last
+		
+
+	end
+
 	def create_user(params)
+		@client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+
+		@question_one = "First, where are you looking- give me one or more neighborhoods: SOMA, FiDi, the Mission, Jackson Square"
+
 		@user = User.create({
     		:phone_number => params[:From],
     		:name => params[:Body],
     		:city => params[:FromCity],
     		:state => params[:FromState]
     	})
+
 		@user.leads.create({})
 
-		@twiml = Twilio::TwiML::Response.new do |r|
-	    r.Message "Hi #{@user.name}, I’m James I’m going to find you space. Just three or four simple questions to start off:"
-	  end
-	  render_twiml @twiml
-	  send_message(@user.phone_number,@question_one)
-	end
-
-	# TODO
-	# Figure out why body variable isn't being passed to Twilio
-	def send_message(to,test)
-    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-    message = @client.messages.create(
+		 welcome_msg = {
     		:from => '+14158010226', 
-        :to => to, 
-        :body => "")
-  end
+        :to => @user.phone_number, 
+        :body => "Hi #{@user.name}, I’m James I’m going to find you space. Just three or four simple questions to start off:"
+      }
+
+    @client.messages.create(welcome_msg)
+	  
+    message_one = {
+    		:from => '+14158010226', 
+        :to => @user.phone_number, 
+        :body => @question_one
+      }
+
+    @client.messages.create(message_one)
+    create_log(params)
+	  render nothing: true
+	end
 
 end
