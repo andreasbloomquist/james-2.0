4dee19cb5cf88f5e3c1c1ff171b07e2af5dd32ee
andreasbloomquist <andreasbloomquist@gmail.com>
2015-07-11 14:19:04 -0700

    Update header style and add edit broker

--- app/controllers/brokers_controller.rb
+++ app/controllers/brokers_controller.rb
@@ -11,22 +11,31 @@ class BrokersController < ApplicationController
 
   def create
     @broker = Broker.new(broker_params)
-
-    respond_to do |format|
-      if @broker.save
-        redirect_to admin_path, success: 'Broker was successfully added.'
-      else
-        format.html { redirect_to new_broker_path alert: @store.errors }
-      end
+    if @broker.save
+      flash[:success] = 'Broker was successfully added.'
+      redirect_to brokers_path
+    else
+      flash[:error] = "#{@broker.errors.each {|e| e }}"
+      redirect_to new_broker_path
     end
   end
+  
+
+  def edit
+    @broker = Broker.find(params[:id])
+  end
+
 
   def destroy
+    @broker = Broker.find(params[:id])
+    @broker.destroy
+    flash[:success] = "Broker was succesfully deleted"
+    redirect_to brokers_path
   end
 
   private
 
   def broker_params
-    params.require(:broker).permit(:first_name, :last_name, :email, :company)
+    params.require(:broker).permit(:first_name, :last_name, :email, :company, :phone_number)
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ActiveRecord::Base
   has_secure_password
-
+  belongs_to :broker
 	def self.confirm(params)
     @admin = Admin.find_by({email: params[:email]})
     @admin.try(:authenticate, params[:password])
