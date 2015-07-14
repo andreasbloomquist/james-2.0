b56af26b164166ce513b706cbccac13c3d2297bb
andreasbloomquist <andreasbloomquist@gmail.com>
2015-07-14 15:22:39 -0700

    remove unused helper

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -5,8 +5,4 @@ module PropertiesHelper
     @prop.update_column(:response_code, code)
   end
 
-  def sanitize_address(address)
-    split_addy = address.split[0..1]
-    return split_addy
-  end
 end
