2f7a62cc9ab3b7bca4b99187296ddb274e2e9194
andreasbloomquist <andreasbloomquist@gmail.com>
2015-07-16 13:02:09 -0700

    remove commented code

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -19,8 +19,6 @@ class SmsController < ApplicationController
       send_property_response(@body)
 
     elsif responding_to_appointment?(sender, @body) && lead_complete?(sender)
-      p "I'm responding to a time to see a property!!!!!!!!!!!"
-      # create_sms_msg(sender, "Got it bro, you're replying to a property. This feature is still in development tho...")
       appt_confirmation(sender, @body)
       render nothing: true
     else
