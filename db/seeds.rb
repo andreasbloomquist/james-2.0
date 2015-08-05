80d3409f3a12632377f9bb195921ce9e14656476
andreasbloomquist <andreasbloomquist@gmail.com>
2015-08-05 16:53:36 -0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
+Broker.destroy_all
+
 # Seeding brokers
+
 Broker.create({
   first_name: "Alex",
   last_name: "Lassar",
-  email: "alex.lassar@gmail.com", 
+  email: "alex.lassar@am.jll.com", 
   phone_number: "+13129275281", 
   company: "JLL"
   })
@@ -13,6 +16,22 @@ Broker.create({
   phone_number: "+16178754788", 
   company: "GA"
   })
+Broker.create({
+  first_name: "Travis",
+  last_name: "James",
+  email: "travis.james@am.jll.com", 
+  phone_number: "+16502480953", 
+  company: "JLL"
+  })
+Broker.create({
+  first_name: "Cody",
+  last_name: "Bishop",
+  email: "cody.bishop@am.jll.com", 
+  phone_number: "+14158193717", 
+  company: "JLL"
+  })
+
+
 
 # Admin.create({
 #   email: "andreasbloomquist@gmail.com",
