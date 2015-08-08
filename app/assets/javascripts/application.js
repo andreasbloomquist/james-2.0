6b80caabcc8cb4cbd3ce0095ae075c59221fcea6
andreasbloomquist <andreasbloomquist@gmail.com>
2015-08-08 16:33:08 -0700

    minor time picker improvements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,13 +7,14 @@
 
 $(document).ready(function(){ 
   $(document).foundation();
+  
   $('.datetimepicker').fdatetimepicker({
     minuteStep: 30,
     autoclose: true,
     linkField: true,
+    showMeridian: true,
     daysOfWeekDisabled: [0,6]
-  })
-
+  });
 
 });
 
@@ -30,3 +31,19 @@ $(window).bind("load", function () {
     }
     $(".uploadcare-widget-button.uploadcare-widget-button-open").addClass("button cust-btn");
 });
+
+var removeAmTimes = function(){
+    $('.hour_am').each(function(){
+        if($(this).html() < 7 || $(this).html() > 11){
+            $(this).remove()
+        }
+    });
+};
+
+var removePmTimes = function(){
+    $('.hour_pm').each(function(){
+        if($(this).html() > 7 && $(this).html() != 12){
+            $(this).remove()
+        }
+    });
+}
